968e33d12642c844af24874f3855847fb6f3d799
Jamie Adkins <jamieadkins95@gmail.com>
2018-01-10 20:54:50 +0000

    Improved output of hook
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- HookRegistry/src/hooks/Gwent.cs
+++ HookRegistry/src/hooks/Gwent.cs
@@ -27,19 +27,24 @@ namespace Hooks
 		object OnCall(string typeName, string methodName, object thisObj, object[] args, IntPtr[] refArgs, int[] refIdxMatch)
 		{
 			if (methodName == "LoadDefinitions") {
+				// Taken from GwentWebServiceClient.Services.GOGRest.LiveDataService.GET_LIVE_DATA_URL
+				string baseUrl = "http://seawolf-config.gog.com/game/{0}_{1}/data/{2}_{3}/data_definitions.zip";
+
 				string hash = ConfigManager.Data.hash;
 				string version = ConfigManager.Data.version;
-
 				string clientVersion = GwentSettings.Instance.ClientVersion;
 				string secret = GwentSettings.Instance.Secret;
-				string[] gwentArgs = new string[] {
-					hash,
-					version,
-					clientVersion,
-					secret
-				};
 
-				File.WriteAllLines("gwent_data_args.txt", gwentArgs);
+				DateTime localDate = DateTime.Now;
+				string date = localDate.ToString();
+				string clientMessage = String.Format("Client Version: {0}", clientVersion);
+				string dataMessage = String.Format("Data Version: {0}", version);
+				string hashMessage = String.Format("Hash: {0}", hash);
+				string secretMessage = String.Format("Secret: {0}", secret);
+
+				string completeUrl = String.Format(baseUrl, clientVersion, secret, version, hash);
+
+				File.WriteAllLines("gwent_data.txt", new string[] { date, clientMessage, dataMessage, hashMessage, secretMessage, completeUrl });
 			}
 
 			return null;
